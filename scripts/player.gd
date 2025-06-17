b75a8d39ac71f4f9bdab52195d68a6db5f61f265
sylvandrift <mattmjohnson33@gmail.com>
2025-06-16 22:28:44 -0700

    implements majority of grid based path finding for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var tile_map = $"../TileMap"
 var astar_grid: AStarGrid2D
+var current_id_path: Array[Vector2i]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,15 @@ func _ready() -> void:
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astar_grid.update()
 	
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(x, y)
+			
+			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
+	
 func _input(event):
 	if event.is_action_pressed("move") == false:
 		return
@@ -17,6 +27,19 @@ func _input(event):
 	var id_path = astar_grid.get_id_path(
 		tile_map.local_to_map(global_position),
 		tile_map.local_to_map(get_global_mouse_position())
-	)
+	).slice(1)
 	
-	print(id_path)
+	if id_path.is_empty() == false:
+		current_id_path = id_path
+		
+func _physics_process(delta):
+	if current_id_path.is_empty():
+		return
+		
+	var target_position = tile_map.map_to_local(current_id_path.front())
+	
+	global_position = global_position.move_toward(target_position, 1)
+	
+	if global_position == target_position:
+		current_id_path.pop_front()
+ 
