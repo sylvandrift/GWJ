f4b11f2cf5199798d21b0b39e6bad4a5b9c29724
SylvanDrift <11220695+sylvandrift@users.noreply.github.com>
2025-06-17 22:15:11 -0700

    grid movement for player and enemy
    * implements majority of grid based path finding for player
    
    * implements remaining work for the movement script of the player
    
    * adds enemy movement script

---
+++ scripts/enemy.gd
@@ -0,0 +1,53 @@
+extends Node2D
+@onready var tile_map = $"../TileMap"
+@onready var player = $"../Player"
+var astar_grid: AStarGrid2D
+var current_id_path: Array[Vector2i]
+var target_position: Vector2
+var is_moving: bool
+var last_player_tile_position: Vector2i
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tile_map.get_used_rect()
+	astar_grid.cell_size = Vector2(16, 16)
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar_grid.update()
+	
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x,
+				y + tile_map.get_used_rect().position.y
+			)
+			
+			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
+		
+func _physics_process(delta):
+	var player_tile_pos = tile_map.local_to_map(player.global_position)
+	
+	if player_tile_pos != last_player_tile_position or current_id_path.is_empty():
+		last_player_tile_position = player_tile_pos
+		var start_pos = tile_map.local_to_map(global_position)
+		var id_path = astar_grid.get_id_path(start_pos, player_tile_pos).slice(1)
+		if id_path.size() > 0:
+			current_id_path = id_path
+			is_moving = true
+	
+	if current_id_path.is_empty():
+		is_moving = false
+		return
+
+	if is_moving:
+		var target_pos = tile_map.map_to_local(current_id_path.front())
+		global_position = global_position.move_toward(target_pos, 100 * delta)
+		
+		if global_position.distance_to(target_pos) < 1.0:
+			global_position = target_pos
+			current_id_path.pop_front()
+ 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,9 @@
 extends Node2D
 @onready var tile_map = $"../TileMap"
 var astar_grid: AStarGrid2D
+var current_id_path: Array[Vector2i]
+var target_position: Vector2
+var is_moving: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,13 +13,55 @@ func _ready() -> void:
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astar_grid.update()
 	
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x,
+				y + tile_map.get_used_rect().position.y
+			)
+			
+			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
+	
 func _input(event):
 	if event.is_action_pressed("move") == false:
 		return
 		
-	var id_path = astar_grid.get_id_path(
-		tile_map.local_to_map(global_position),
-		tile_map.local_to_map(get_global_mouse_position())
-	)
+	var id_path
 	
-	print(id_path)
+	if is_moving:
+		id_path = astar_grid.get_id_path(
+			tile_map.local_to_map(target_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		)
+	else:
+		id_path = astar_grid.get_id_path(
+			tile_map.local_to_map(global_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		).slice(1)
+	
+	if id_path.is_empty() == false:
+		current_id_path = id_path
+		
+func _physics_process(delta):
+	if current_id_path.is_empty():
+		return
+		
+	if is_moving == false:
+		target_position = tile_map.map_to_local(current_id_path.front())
+		is_moving = true
+		
+	var target_position = tile_map.map_to_local(current_id_path.front())
+	
+	global_position = global_position.move_toward(target_position, 1)
+	
+	if global_position == target_position:
+		current_id_path.pop_front()
+		
+		if current_id_path.is_empty() == false:
+			target_position = tile_map.map_to_local(current_id_path.front())
+		else:
+			is_moving = false
+ 
